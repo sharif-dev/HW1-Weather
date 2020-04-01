818357d3b3c75a1197b885d102c8e577f8c00482
mgh_2000 <m.aghasemzadeh@yahoo.com>
2020-04-01 14:44:10 +0430

    enable proguard

--- app/src/main/java/com/sharifdev/weather/MainActivity.java
+++ app/src/main/java/com/sharifdev/weather/MainActivity.java
@@ -1,18 +1,40 @@
 package com.sharifdev.weather;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.downloader.Error;
+import com.downloader.OnCancelListener;
+import com.downloader.OnDownloadListener;
+import com.downloader.OnPauseListener;
+import com.downloader.OnProgressListener;
+import com.downloader.OnStartOrResumeListener;
+import com.downloader.PRDownloader;
+import com.downloader.PRDownloaderConfig;
+import com.downloader.Progress;
+import com.downloader.utils.Utils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Environment;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mDownloadBtn;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Setting timeout globally for the download network requests:
+        PRDownloaderConfig config = PRDownloaderConfig.newBuilder().build();
+        PRDownloader.initialize(getApplicationContext(), config);
+
+        mDownloadBtn = findViewById(R.id.activity_main_download_button);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,7 +57,28 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        mDownloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://irsv.upmusics.com/99/Hamed%20Zamani%20%7C%20Sepidar%20(128).mp3";
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setDescription("Download test");
+                request.setTitle("Download title");
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "download.mp3");
+
+                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                manager.enqueue(request);
+
+            }
+        });
+        
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
