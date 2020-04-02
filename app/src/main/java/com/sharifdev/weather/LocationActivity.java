e66eb5e2107ea9aa38c9116cf401a97fbf3640bb
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-04-02 15:38:54 +0430

    searchable spinner added

--- app/src/main/java/com/sharifdev/weather/LocationActivity.java
+++ app/src/main/java/com/sharifdev/weather/LocationActivity.java
@@ -3,12 +3,46 @@ package com.sharifdev.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class LocationActivity extends AppCompatActivity {
+    String[] options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        Spinner spinner = findViewById(R.id.CitySelectSpinner);
+        // Creating ArrayAdapter using the string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities, android.R.layout.simple_spinner_item);
+        // Specify layout to be used when list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Applying the adapter to our spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        options = LocationActivity.this.getResources().getStringArray(R.array.cities);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this, " You select >> " + options[position], Toast.LENGTH_SHORT).show();
+
     }
 }
