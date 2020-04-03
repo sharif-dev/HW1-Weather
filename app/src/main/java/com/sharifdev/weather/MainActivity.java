d12e1174e8b945394b3413a8f7bc30861023085a
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-04-03 07:39:46 +0430

    location activity style changed

--- app/src/main/java/com/sharifdev/weather/MainActivity.java
+++ app/src/main/java/com/sharifdev/weather/MainActivity.java
@@ -2,16 +2,13 @@ package com.sharifdev.weather;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
