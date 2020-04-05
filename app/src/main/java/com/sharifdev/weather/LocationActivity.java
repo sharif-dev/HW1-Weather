47c810d84f7ea6216fbb21cbe15a2ceeaed48f10
mhbahmani <mhbahmani79@gmail.com>
2020-04-05 16:09:21 +0430

    add getting weather from api

--- app/src/main/java/com/sharifdev/weather/LocationActivity.java
+++ app/src/main/java/com/sharifdev/weather/LocationActivity.java
@@ -1,8 +1,10 @@
 package com.sharifdev.weather;
 
 import android.os.Bundle;
+import android.telephony.mbms.MbmsErrors;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -11,13 +13,17 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.sharifdev.weather.models.CitiesResponse;
-import com.sharifdev.weather.models.City;
+import com.sharifdev.weather.models.coordination.CitiesResponse;
+import com.sharifdev.weather.models.coordination.City;
+import com.sharifdev.weather.models.weather.WeatherResponse;
 import com.sharifdev.weather.network.Mapbox;
 import com.sharifdev.weather.network.RetrofitClient;
+import com.sharifdev.weather.network.WeatherAPI;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,7 +45,7 @@ public class LocationActivity extends AppCompatActivity {
         adapter = new CityAdapter(this, citiesList);
         listview.setAdapter(adapter);
 
-        RetrofitClient mapboxClient = new RetrofitClient(getString(R.string.mapbox_url));
+        RetrofitClient mapboxClient = new RetrofitClient(getString(R.string.mapbox_api));
         final Mapbox service = mapboxClient.getRetrofit().create(Mapbox.class);
 
         Search.addTextChangedListener(new TextWatcher() {
@@ -50,7 +56,7 @@ public class LocationActivity extends AppCompatActivity {
                 String[] cities;
                 if (cs.length() != 0) {
                     // Getting cities list from mapbox api
-                    Call<CitiesResponse> call = service.getWeather(cs.toString(), getString(R.string.mapbox_token));
+                    Call<CitiesResponse> call = service.getCities(cs.toString(), getString(R.string.mapbox_token));
                     call.enqueue(new Callback<CitiesResponse>() {
                         @Override
                         public void onResponse(Call<CitiesResponse> call, Response<CitiesResponse> response) {
@@ -96,6 +102,31 @@ public class LocationActivity extends AppCompatActivity {
                 currentCity.setText(clickedCity.getRealName());
                 latitude.setText(String.format("Latitude: %f, ", clickedCity.getCoordinates().get(1)));
                 longitude.setText(String.format("Longitude: %f, ", clickedCity.getCoordinates().get(0)));
+
+
+                final RetrofitClient weatherClient = new RetrofitClient(getString(R.string.weather_api));
+                final WeatherAPI service = weatherClient.getRetrofit().create(WeatherAPI.class);
+
+                List<Double> coordinates = clickedCity.getCoordinates();
+                Collections.reverse(coordinates);
+                Call<WeatherResponse> call = service.getWeather(
+                        coordinates,
+                        getString(R.string.weather_token));
+
+                call.enqueue(new Callback<WeatherResponse>() {
+                    @Override
+                    public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                        WeatherResponse weatherResponse = response.body();
+                        System.out.print(weatherResponse.getLocation().getName() + "  ");
+                        System.out.println(weatherResponse.getCurrentSummeryWeather().getTemperature());
+                        System.out.println(weatherResponse.getCurrentSummeryWeather().getCondition());
+                    }
+
+                    @Override
+                    public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                        Log.e("weather", Objects.requireNonNull(t.getMessage()));
+                    }
+                });
 
                 // TODO: Save selected City
             }
