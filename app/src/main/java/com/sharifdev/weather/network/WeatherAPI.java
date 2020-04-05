9f1ae4d0fd93fba942392326d7a883e36acf3c01
mhbahmani <mhbahmani79@gmail.com>
2020-04-05 16:11:12 +0430

    add Weather endpoint

--- app/src/main/java/com/sharifdev/weather/network/WeatherAPI.java
+++ app/src/main/java/com/sharifdev/weather/network/WeatherAPI.java
@@ -9,6 +9,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface WeatherAPI {
-    @GET("forecast.json")
-    Call<WeatherResponse> getWeather(@Query("q") List<Double> q, @Query("key") String key, @Query("days") int days);
+    @GET("forecast.json?days=7")
+    Call<WeatherResponse> getWeather(@Query("q") List<Double> q, @Query("key") String key);
 }
