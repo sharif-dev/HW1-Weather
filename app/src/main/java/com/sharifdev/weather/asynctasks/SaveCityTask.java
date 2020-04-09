a756601dbdab23f665fba70f08583645b3697da7
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2020-04-09 19:54:03 +0430

    refactor calling LoadCityTask and SaveCityTask

--- app/src/main/java/com/sharifdev/weather/asynctasks/SaveCityTask.java
+++ app/src/main/java/com/sharifdev/weather/asynctasks/SaveCityTask.java
@@ -6,24 +6,30 @@ import android.os.AsyncTask;
 
 import com.google.gson.Gson;
 import com.sharifdev.weather.R;
+import com.sharifdev.weather.datamodels.CityDataCallback;
 import com.sharifdev.weather.models.coordination.City;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collections;
 
 
 public class SaveCityTask extends AsyncTask<City, Void, Integer> {
     @SuppressLint("StaticFieldLeak")
     private Context context;
+    private CityDataCallback callback;
+    private City city;
 
-    public SaveCityTask(Context context) {
+    public SaveCityTask(Context context, CityDataCallback callback) {
         this.context = context;
+        this.callback = callback;
     }
 
     @Override
     protected Integer doInBackground(City... cities) {
         Gson gson = new Gson();
         City city = cities[0];
+        this.city = city;
         String json = gson.toJson(city);
         FileOutputStream outputStream;
         try {
@@ -36,5 +42,10 @@ public class SaveCityTask extends AsyncTask<City, Void, Integer> {
             e.printStackTrace();
         }
         return 200;
+    }
+
+    @Override
+    protected void onPostExecute(Integer integer) {
+        this.callback.onComplete(Collections.singletonList(city));
     }
 }
--- app/src/main/java/com/sharifdev/weather/datamodels/CityData.java
+++ app/src/main/java/com/sharifdev/weather/datamodels/CityData.java
@@ -3,16 +3,11 @@ package com.sharifdev.weather.datamodels;
 import android.content.Context;
 
 import com.sharifdev.weather.asynctasks.LoadCityTask;
+import com.sharifdev.weather.asynctasks.SaveCityTask;
 import com.sharifdev.weather.models.coordination.CitiesResponse;
 import com.sharifdev.weather.models.coordination.City;
 import com.sharifdev.weather.network.Mapbox;
 import com.sharifdev.weather.network.RetrofitClient;
-import com.sharifdev.weather.asynctasks.SaveCityTask;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicReference;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -21,7 +16,7 @@ import retrofit2.Response;
 public class CityData {
     private Context context;
     private static CityData instance = new CityData();
-    private City city = new City();
+    private City city;
 
     public static CityData getInstance(Context context) {
         instance.context = context;
@@ -29,26 +24,18 @@ public class CityData {
     }
 
     public void loadCity(CityDataCallback callback) {
-        final AtomicReference<LoadCityTask> loadCityTask = new AtomicReference<>();
-        loadCityTask.getAndSet(new LoadCityTask(this.context));
-        try {
-            City city = loadCityTask.get().execute().get();
-            setCity(city);
-            System.out.println("loaded" + city.getRealName() + city.getCoordinates());
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        LoadCityTask loadCityTask = new LoadCityTask(this.context, callback);
+        loadCityTask.execute();
 
-        callback.onComplete(Collections.singletonList(city));
+//        callback.onComplete(Collections.singletonList(city));
     }
 
     public void saveCity(final City city, CityDataCallback callback) {
-        final AtomicReference<SaveCityTask> saveCityTask = new AtomicReference<>();
-        saveCityTask.getAndSet(new SaveCityTask(this.context));
-        saveCityTask.get().execute(city);
-        System.out.println("saved" + city.getRealName() + city.getCoordinates());
+        SaveCityTask saveCityTask = new SaveCityTask(this.context, callback);
+        saveCityTask.execute(city);
+        System.out.println("saving" + city.getRealName() + city.getCoordinates());
 
-        callback.onComplete(Collections.singletonList(city));
+//        callback.onComplete(Collections.singletonList(city));
     }
 
     public void searchCities(String query, String api, String token, final CityDataCallback callback) {
